4f6b3b43ad27529fbba19ce1ea55abd12b511718
Rasika Athavale <rasika.athavale@gmail.com>
2020-02-29 21:48:38 -0800

    Classification and using Alpaca to verify ticker name

--- stonksRedditPart.py
+++ stonksRedditPart.py
@@ -1,90 +1,77 @@
 import praw
-import pandas as pd
-import requests
-import stocker
-from bs4 import BeautifulSoup
+from nltk.corpus import words as nltkwords
+import alpaca_trade_api as tradeapi
+import config
 
-def getScore(sub):
-    return sub.score
+class tickerSuggestion:
 
-def getDDPostTitles():
-    reddit = praw.Reddit(client_id='4AKaie_Ao4jaWQ',
-                        client_secret='79N7FMADsGXgCvF0LqZJEiMtkg4',
-                        user_agent='script:StonksPredictor')
+    def __init__(self, PRAW_CLIENT_ID, PRAW_CLIENT_SECRET, PRAW_USER_AGENT, ALPACA_KEY_ID, ALPACA_SECRET):
+        self.api = tradeapi.REST(ALPACA_KEY_ID, ALPACA_SECRET, api_version='v2')
+        self.reddit = praw.Reddit(client_id=PRAW_CLIENT_ID, client_secret=PRAW_CLIENT_SECRET,
+        user_agent= PRAW_USER_AGENT)
 
-    ddSubs = []
-    for submission in reddit.subreddit('wallstreetbets').hot():
-        if submission.link_flair_text == 'DD':
-            ddSubs.append(submission)
-    
-    for submission in reddit.subreddit('wallstreetbets').new():
-        if submission.link_flair_text == 'DD':
-            ddSubs.append(submission)
+    def _getScore(self, sub):
+        return sub.score
 
-    ddSubs.sort(key = getScore, reverse=True)
-    ddSubsTitles = [sub.title for sub in ddSubs]
-    return ddSubsTitles
-
-def validateSP500(tickerName):
-    table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    df = table[0]
-
-    for sp500Ticker in df['Symbol']:
-        if tickerName == sp500Ticker:
-            return True
-    return False
-
-def getTickers(ddSubsTitles):
-
-    allResults = set() 
-
-    blacklist_words = [
-      "YOLO", "TOS", "CEO", "CFO", "CTO", "DD", "BTFD", "WSB", "OK", "RH",
-      "KYS", "FD", "TYS", "US", "USA", "IT", "ATH", "RIP", "BMW", "GDP",
-      "OTM", "ATM", "ITM", "IMO", "LOL", "DOJ", "BE", "PR", "PC", "ICE",
-      "TYS", "ISIS", "PRAY", "PT", "FBI", "SEC", "GOD", "NOT", "POS", "COD",
-      "AYYMD", "FOMO", "TL;DR", "EDIT", "STILL", "LGMA", "WTF", "RAW", "PM",
-      "LMAO", "LMFAO", "ROFL", "EZ", "RED", "BEZOS", "TICK", "IS", "DOW"
-      "AM", "PM", "LPT", "GOAT", "FL", "CA", "IL", "PDFUA", "MACD", "HQ",
-      "OP", "DJIA", "PS", "AH", "TL", "DR", "JAN", "FEB", "JUL", "AUG",
-      "SEP", "SEPT", "OCT", "NOV", "DEC", "FDA", "IV", "ER", "IPO", "RISE"
-      "IPA", "URL", "MILF", "BUT", "SSN", "FIFA", "USD", "CPU", "AT",
-      "GG", "ELON", "TO", "SEE", "THE", "ON", "YOU", "IS", "BUY", "LONG", "SHORT", "POOR", "RICH",
-      "DEAL", "ALL", "BAD", "PUTS", "VS", "V", "A", "FOR"
-   ]
-
-    for titles in ddSubsTitles:
-        words = [w for w in titles.split() if len(w) < 5 and w.upper() not in blacklist_words and w.isalpha()]
+    def getDDPostTitles(self):
+        ddSubs = []
+        for submission in self.reddit.subreddit('wallstreetbets').hot():
+            if submission.link_flair_text == 'DD':
+                ddSubs.append(submission)
         
-        for ticker in words:
-            try:
-                if wordnet.synsets(ticker):
-                    continue
-                elif len(stocker.predict.tomorrow(ticker)) == 0:
-                    continue
-                elif len(allResults) < 4:
-                    allResults.add(ticker)
-                else:
-                    break
-            except:
-                continue
-    
-    return allResults
+        for submission in self.reddit.subreddit('wallstreetbets').new():
+            if submission.link_flair_text == 'DD':
+                ddSubs.append(submission)
 
-def printContents(collection):
-    print("-------------------")
-    for elem in collection:
-        print(elem)
-    print("-------------------")
+        ddSubs.sort(key = self._getScore, reverse=True)
+        ddSubsTitles = [sub.title for sub in ddSubs]
+        return ddSubsTitles
 
-def main():
-    ddSubsTitles = getDDPostTitles()
-    #printContents(ddSubsTitles)
-    allTickers = getTickers(ddSubsTitles)
-    printContents(allTickers)
+    def getTickers(self, ddSubsTitles):
+        allResults = set()
+
+        blacklist_words = [
+          "YOLO", "TOS", "CEO", "CFO", "CTO", "DD", "BTFD", "WSB", "OK", "RH",
+          "KYS", "FD", "TYS", "US", "USA", "IT", "ATH", "RIP", "BMW", "GDP",
+          "OTM", "ATM", "ITM", "IMO", "LOL", "DOJ", "BE", "PR", "PC", "ICE",
+          "TYS", "ISIS", "PRAY", "PT", "FBI", "SEC", "GOD", "NOT", "POS", "COD",
+          "AYYMD", "FOMO", "TL;DR", "EDIT", "STILL", "LGMA", "WTF", "RAW", "PM",
+          "LMAO", "LMFAO", "ROFL", "EZ", "RED", "BEZOS", "TICK", "IS", "DOW"
+          "AM", "PM", "LPT", "GOAT", "FL", "CA", "IL", "PDFUA", "MACD", "HQ",
+          "OP", "DJIA", "PS", "AH", "TL", "DR", "JAN", "FEB", "JUL", "AUG",
+          "SEP", "SEPT", "OCT", "NOV", "DEC", "FDA", "IV", "ER", "IPO", "RISE"
+          "IPA", "URL", "MILF", "BUT", "SSN", "FIFA", "USD", "CPU", "AT",
+          "GG", "ELON", "TO", "SEE", "THE", "ON", "YOU", "IS", "BUY", "LONG", "SHORT", "POOR", "RICH",
+          "DEAL", "ALL", "BAD", "PUTS", "VS", "V", "A", "FOR"
+       ] + nltkwords.words()
+
+        for titles in ddSubsTitles:
+            word_list = [w for w in titles.split() if len(w) < 5 and w.upper() not in blacklist_words and w.isalpha()]
+            
+            for ticker in word_list:
+                try:
+                    ret = self.api.get_asset(ticker)
+                    if len(allResults) < 4:
+                        allResults.add(ticker)
+                    else:
+                        break
+                except:
+                    continue
+        
+        return allResults
+
+    def printContents(self, collection):
+        print("-------------------")
+        for elem in collection:
+            print(elem)
+        print("-------------------")
 
 if __name__ == "__main__":
-    main()
+    stockSuggestions = tickerSuggestion(config.PRAW_CLIENT_ID, config.PRAW_CLIENT_SECRET, config.PRAW_USER_AGENT, config.ALPACA_KEY_ID, config.ALPACA_SECRET)
+    ddSubsTitles = stockSuggestions.getDDPostTitles()
+    #stockSuggestions.printContents(ddSubsTitles)
+    allTickers = stockSuggestions.getTickers(ddSubsTitles)
+    stockSuggestions.printContents(allTickers)
 
 
         
